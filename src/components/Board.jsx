7c297cffd4f8438f851ef9293d1d76a62966b563
Ishaan-11 <ishaansharma1998@gmail.com>
2021-01-02 16:40:17 +0530

    lifting-state-up-again

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,65 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Square from './Square';
 
-function Board() {
-
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setNext] = useState(true);
-  const winner = calculateWinner(squares);
-  let status;
-
-  if (winner) {
-    status = 'Winner: ' + winner;
-  } else if (!squares.includes(null)) {
-    status = 'Draw!';
-  } else {
-    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
-  }
-
-  function handleClick(i) {
-    const newSquares = squares.slice();
-
-    if (calculateWinner(newSquares) || squares[i]) {
-      return;
-    }
-
-    newSquares[i] = xIsNext ? 'X' : 'O';
-    setSquares(newSquares);
-    setNext(prevValue => !prevValue);
-  }
+function Board(props) {
 
   function renderSquare(i) {
     return (
       <Square 
-        value={squares[i]}
-        onClick={() => handleClick(i)}
+        value={props.squares[i]}
+        onClick={() => props.onClick(i)}
       />
     );
   }
 
-  function calculateWinner(grid) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (grid[a] && grid[a] === grid[b] && grid[a] === grid[c]) {
-        return grid[a];
-      }
-    }
-    return null;
-  }
-
   return (
     <div>
-      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,14 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Board from './Board';
 
 function Game() {
+  const [xIsNext, setNext] = useState(true);
+  const [history, setHistory] = useState([{
+    squares: Array(9).fill(null)
+  }]);
+
+  const historyCopy = history;
+  const current = historyCopy[historyCopy.length - 1];
+  const winner = calculateWinner(current.squares);
+  let status;
+
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else if (!current.squares.includes(null)) {
+    status = 'Draw!';
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+  }
+
+  function calculateWinner(grid) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (grid[a] && grid[a] === grid[b] && grid[a] === grid[c]) {
+        return grid[a];
+      }
+    }
+    return null;
+  }
+
+  function handleClick(i) {
+    const historyCopy = history;
+    const current = historyCopy[historyCopy.length - 1];
+    const newSquares = current.squares.slice();
+
+    if (calculateWinner(newSquares) || current[i]) {
+      return;
+    }
+
+    newSquares[i] = xIsNext ? 'X' : 'O';
+    setHistory(prevValue => {
+      return [...prevValue, {squares: newSquares}];
+    });
+    setNext(prevValue => !prevValue);
+  }
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board 
+          squares={current.squares}
+          onClick={handleClick}
+        />
       </div>
       <div className="game-info">
-        <div>{/* status */}</div>
+        <div>{status}</div>
         <ol>{/* TODO */}</ol>
       </div>
     </div>
